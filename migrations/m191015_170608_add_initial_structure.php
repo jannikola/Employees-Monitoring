400bf77620a635389b94e50d6c700b0a02871b91
jannikola <jannikola@gmail.com>
2019-10-15 19:46:05 +0200

    initial database structure migration

---
+++ migrations/m191015_170608_add_initial_structure.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191015_170608_add_initial_structure
+ */
+class m191015_170608_add_initial_structure extends Migration
+{
+
+    public function safeUp()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255)->notNull(),
+            'email' => $this->string(255)->notNull(),
+            'created_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'updated_by' => $this->integer(),
+            'is_deleted' => $this->integer(),
+        ]);
+
+        $this->createTable('employee', [
+            'id' => $this->primaryKey(),
+            'first_name' => $this->string(255),
+            'last_name' => $this->string(255),
+            'created_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'updated_by' => $this->integer(),
+            'is_deleted' => $this->integer(),
+        ]);
+
+        $this->createTable('arrival', [
+            'id' => $this->primaryKey(),
+            'employee_id' => $this->integer(),
+            'date' => $this->integer(),
+            'time' => $this->integer(),
+            'is_late' => $this->integer(),
+            'created_at' => $this->integer(),
+            'created_by' => $this->integer(),
+            'updated_at' => $this->integer(),
+            'updated_by' => $this->integer(),
+            'is_deleted' => $this->integer(),
+        ]);
+
+        $this->addForeignKey('fk_arrival_employee', 'arrival', 'employee_id', 'employee', 'id');
+    }
+
+
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_arrival_employee', 'arrival');
+        
+        $this->dropTable('arrival');
+        $this->dropTable('employee');
+        $this->dropTable('user');
+
+    }
+
+
+}
